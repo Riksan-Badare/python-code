c77b5149b2102479768fe401210efda666b90858
Riksan Badare <rikbar.creator@gmail.com>
2022-01-11 20:54:21 +0800

    perulangan

---
+++ dasar_sintaksis_perulangan_baca_buku.py
@@ -0,0 +1,15 @@
+"""
+program perulangan membaca buku
+"""
+jumlah_buku = 10
+
+print('Ibu berkata, baca semua bukumu')
+
+jumlah_buku_yang_sudah_dibaca = 0
+print(f"jumlah buku yang sudah dibaca{jumlah_buku_yang_sudah_dibaca}")
+
+for jumlah_buku_yang_sudah_dibaca in range (1, 11):
+    print (f"Buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+
+print (jumlah_buku_yang_sudah_dibaca)
+
