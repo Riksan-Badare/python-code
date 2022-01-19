ffa98b0e58b7196f0c0734d1c1f196dfba43ed67
Riksan Badare <rikbar.creator@gmail.com>
2022-01-19 14:40:13 +0800

    Menggunakan list

--- dasar_sintaksis_list_comprehension.py
+++ dasar_sintaksis_list_comprehension.py
@@ -83,37 +83,3 @@ del daftar_buku[0::2] #start:end:step, minus dihapus dari belakang
 for i in range (0, len(daftar_buku)):
     print(daftar_buku[i])
 
-print('\nMembuat list baru')
-daftar_buku = ['Seven Habits','How to Influence People','First Things First', '4DX']
-daftar_buku_baru = daftar_buku[:]
-for i in range (0, len(daftar_buku)):
-    print(daftar_buku[i])
-
-print('\nMembuat list baru')
-del daftar_buku[:]
-for i in range (0, len(daftar_buku_baru)):
-    print(daftar_buku_baru[i])
-
-print('\nMembuat list baru dengan comprehension : ganjil')
-daftar_buku = ['1 Seven Habits','2 How to Influence People','3 First Things First', '4 4DX']
-daftar_buku_baru = daftar_buku[0::2]
-for i in range (0, len(daftar_buku_baru)):
-    print(daftar_buku_baru[i])
-
-print('\nMembuat list baru dengan comprehension : genap')
-daftar_buku = ['1 Seven Habits','2 How to Influence People','3 First Things First', '4 4DX']
-daftar_buku_baru = daftar_buku[1::2]#start:stop:step
-for i in range (0, len(daftar_buku_baru)):
-    print(daftar_buku_baru[i])
-
-print('\nMembuat list baru dengan comprehension : buang di ujung')
-daftar_buku = ['1 Seven Habits','2 How to Influence People','3 First Things First', '4 4DX']
-daftar_buku_baru = daftar_buku [0:-1:2] #minus ambil dari belakang buang 1
-for i in range (0, len(daftar_buku_baru)):
-    print(daftar_buku_baru[i])
-
-#BISA JUGA LANGSUNG DITAMPILKAN TANPA FOR I
-print('\nMembuat list baru dengan comprehension : buang di ujung')
-daftar_buku = ['1 Seven Habits','2 How to Influence People','3 First Things First', '4 4DX']
-print(daftar_buku[0::2])
-
