0facd1def4f9e5ad7c427cbd82d0ed8db5e8fb7c
Riksan Badare <rikbar.creator@gmail.com>
2022-02-03 23:27:04 +0800

    Menggunakan list

---
+++ dasar_sintaksis+dictionary.py
@@ -0,0 +1,11 @@
+users = {
+    'id' : 1,
+    'name' : 'Leanne Graham',
+    'username' : 'Bret',
+    'email' : 'Sincere@april.biz'
+} #jangan menggunakan angka agar dapat dijadikan Json
+print(users)
+print(users['name'])
+print(users['name'])
+print(users['username'])
+print(users['email'])
