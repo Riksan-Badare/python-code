60ae82a6dd18fd0d813500fe4cbabeb71288f4cb
Riksan Badare <rikbar.creator@gmail.com>
2022-02-03 23:43:40 +0800

    Menggunakan list

--- dasar_sintaksis+dictionary.py
+++ dasar_sintaksis+dictionary.py
@@ -1,11 +1,33 @@
 users = {
-    'id' : 1,
-    'name' : 'Leanne Graham',
-    'username' : 'Bret',
-    'email' : 'Sincere@april.biz'
+    "id" : 1,
+    "name" : "Leanne Graham",
+    "username" : "Bret",
+    "email" : "Sincere@april.biz",
+    "address" : {
+        "street" : "Kulas light",
+        "suite" : "Apt. 556",
+        "city" : "Gwenborough",
+        "zipcode" : "92998-3874",
+        "geo" : {
+                "lat" : "-37.3159",
+                "lng" : "81.1496"
+                    }
+            }
 } #jangan menggunakan angka agar dapat dijadikan Json
 print(users)
-print(users['name'])
-print(users['name'])
-print(users['username'])
-print(users['email'])
+print(users["id"])
+print(users["name"])
+print(users["username"])
+print(users["email"])
+print(users["address"])
+print(users["address"]["street"])
+print(users["address"]["geo"]["lat"])
+print(users["address"]["geo"]["lng"])
+
+print(users)
+print(type(users))#cek tipe data
+print("'nUbah dict ke Json")
+import json
+result = json.dumps(users)
+print(type(result))#cek tipe data
+print(result)
